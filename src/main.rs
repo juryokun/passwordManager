9a245c56d27a8a5b6e569493e7e4869df06fa53a
juryokun <capeta.kozo@gmail.com>
2020-10-18 22:52:41 +0900

    2020/10/18

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
+use std::fs::File;
 use std::io;
 use std::process;
 
-fn read() -> Result<(), Box<dyn Error>> {
-    let mut rdr = csv::Reader::from_reader(io::stdin());
+fn read(data: File) -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_reader(data);
     for result in rdr.records() {
         let records = result?;
         let title = &records[0];
@@ -15,7 +16,8 @@ fn read() -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
-    if let Err(err) = read() {
+    let data_csv = File::open("data.csv");
+    if let Err(err) = read(data_csv.unwrap()) {
         println!("error running read: {}", err);
         process::exit(1);
     }
